e95865a148045fca58db1df1843e43b77237fbca
deependra <sdeependra216@gmail.com>
2024-10-05 21:52:07 +0530

    src

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 style={{display:"flex", justifyContent:"center",alignItems:"center"}}>
+        Vite + React
+        </h1>
+      
+    </>
+  )
+}
+
+export default App
