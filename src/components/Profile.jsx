01c89547759a995f241e0f52c198a03194f976b3
deependra <sdeependra216@gmail.com>
2024-10-06 18:11:54 +0530

    not login without auth

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const user =useSelector((store)=>store.user)
+
   return (
     <div>
-        Profile Page      
+      <h1>Profile Page</h1>
+        <div>Name: {user.firstName}</div>      
+        <div>Last: {user.lastName}</div>      
+        {/* <div>Name: {user.firstName}</div>       */}
+
     </div>
   )
 }
